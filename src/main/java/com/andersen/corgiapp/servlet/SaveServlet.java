9e863e2793f134c198672dfd69a90bcfaaecc90c
MerakseS <the.sergei.losev@gmail.com>
2022-12-28 17:24:19 +0300

    #1 Add Exception handler

--- src/main/java/com/andersen/corgiapp/servlet/SaveServlet.java
+++ src/main/java/com/andersen/corgiapp/servlet/SaveServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.andersen.corgiapp.entity.User;
 import com.andersen.corgiapp.service.UserService;
 import com.andersen.corgiapp.service.UserServiceImpl;
@@ -15,13 +18,18 @@ import com.andersen.corgiapp.service.UserServiceImpl;
 @WebServlet(name = "SaveServlet", value = "/users/save")
 public class SaveServlet extends HttpServlet {
 
+    private static final Logger log = LoggerFactory.getLogger(SaveServlet.class);
+
+    private static final String USER_LIST_PATH = "/users";
+    private static final String NEW_USER_FORM_PATH = "/users/new";
+
     private static final String NAME_PARAMETER = "name";
     private static final String SURNAME_PARAMETER = "surname";
     private static final String AGE_PARAMETER = "age";
 
-    private final UserService userService;
+    private static final String ERROR_ATTRIBUTE_NAME = "errorMessage";
 
-    private static final String USER_LIST_PATH = "/users";
+    private final UserService userService;
 
     public SaveServlet() {
         userService = new UserServiceImpl();
@@ -29,13 +37,20 @@ public class SaveServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = new User();
-        user.setName(request.getParameter(NAME_PARAMETER));
-        user.setSurname(request.getParameter(SURNAME_PARAMETER));
-        user.setAge(Integer.parseInt(request.getParameter(AGE_PARAMETER)));
+        try {
+            User user = new User();
+            user.setName(request.getParameter(NAME_PARAMETER));
+            user.setSurname(request.getParameter(SURNAME_PARAMETER));
+            user.setAge(Integer.parseInt(request.getParameter(AGE_PARAMETER)));
 
-        userService.save(user);
+            userService.save(user);
 
-        response.sendRedirect(USER_LIST_PATH);
+            response.sendRedirect(USER_LIST_PATH);
+        }
+        catch (RuntimeException e) {
+            log.warn("Can't create user cause: ", e);
+            request.setAttribute(ERROR_ATTRIBUTE_NAME, e.getMessage());
+            request.getRequestDispatcher(NEW_USER_FORM_PATH).forward(request, response);
+        }
     }
 }
--- src/main/webapp/WEB-INF/jsp/addUser.jsp
+++ src/main/webapp/WEB-INF/jsp/addUser.jsp
@@ -1,25 +1,29 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
+<jsp:useBean id="errorMessage" class="java.lang.String" scope="request"/>
+
 <html>
 <head>
     <title>Добавить пользователя</title>
 </head>
 <body>
-    <form action="save" method="post">
-        <label for="name">Имя</label>
-        <input id="name" name="name" type="text">
-        <br><br>
-        <label for="surname">Фамилия</label>
-        <input id="surname" name="surname" type="text">
-        <br><br>
-        <label for="age">Возраст</label>
-        <input id="age" name="age" type="number" min="0">
-        <br><br>
-        <button type="submit">Добавить пользователя</button>
-        <a href="<c:url value="/users"/> ">
-            <button>Отмена</button>
-        </a>
-    </form>
+<form action="save" method="post">
+    <label for="name">Name</label>
+    <input id="name" name="name" type="text">
+    <br><br>
+    <label for="surname">Surname</label>
+    <input id="surname" name="surname" type="text">
+    <br><br>
+    <label for="age">Age</label>
+    <input id="age" name="age" type="number" min="0" value="0">
+    <br><br>
+
+    <button type="submit">Add user</button>
+    <a href="<c:url value="/users"/> ">
+        <button>Cancel</button>
+    </a>
+    <p><%= errorMessage%></p>
+</form>
 </body>
 </html>
