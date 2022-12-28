4bda20cdd66e31f5f071a1fd0fc257139a1a2179
MerakseS <the.sergei.losev@gmail.com>
2022-12-28 17:58:01 +0300

    #1 Add UserServiceImpl Test

--- src/main/java/com/andersen/corgiapp/entity/User.java
+++ src/main/java/com/andersen/corgiapp/entity/User.java
@@ -7,6 +7,15 @@ public class User {
     private String surname;
     private int age;
 
+    public User() {
+    }
+
+    public User(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
     public long getId() {
         return id;
     }
