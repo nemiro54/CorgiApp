eb7cf6dd4b0a854f86790d6f60872e1ad6a891aa
Uladzislau Niamira <nemiro54@gmail.com>
2022-12-30 15:48:07 +0300

    4 (review): updated EditUserFormServlet.java to throw error message to list of users

--- src/main/java/com/andersen/corgiapp/servlet/EditUserFormServlet.java
+++ src/main/java/com/andersen/corgiapp/servlet/EditUserFormServlet.java
@@ -6,6 +6,8 @@ import com.andersen.corgiapp.repository.UserRepository;
 import com.andersen.corgiapp.repository.UserRepositoryImpl;
 import com.andersen.corgiapp.service.UserService;
 import com.andersen.corgiapp.service.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -15,11 +17,15 @@ import java.io.IOException;
 @WebServlet(name = "EditUserFormServlet", value = "/users/edit")
 public class EditUserFormServlet extends HttpServlet {
 
+    private static final Logger log = LoggerFactory.getLogger(EditUserFormServlet.class);
+
     public static final String USER_EDIT_PATH = "/WEB-INF/jsp/editUser.jsp";
     private static final String USER_LIST_PATH = "/users";
 
     private static final String USER_PARAMETER = "user";
     private static final String ID_PARAMETER = "id";
+
+    private static final String ERROR_ATTRIBUTE_NAME = "errorMessage";
 
     private final UserService userService;
 
@@ -37,6 +43,8 @@ public class EditUserFormServlet extends HttpServlet {
             request.setAttribute(USER_PARAMETER, user);
             request.getRequestDispatcher(USER_EDIT_PATH).forward(request, response);
         } catch (ModelNotFoundException e) {
+            log.warn("Can't update user cause: ", e);
+            request.setAttribute(ERROR_ATTRIBUTE_NAME, e.getMessage());
             request.getRequestDispatcher(USER_LIST_PATH).forward(request, response);
         }
     }
