e139e995813528c0b5e57127a9163ae2301a975c
Uladzislau Niamira <nemiro54@gmail.com>
2022-12-30 12:43:45 +0300

    4 (review): added new servlet EditUserFormServlet.java after code review

---
+++ src/main/java/com/andersen/corgiapp/servlet/EditUserFormServlet.java
@@ -0,0 +1,42 @@
+package com.andersen.corgiapp.servlet;
+
+import com.andersen.corgiapp.entity.User;
+import com.andersen.corgiapp.repository.UserRepository;
+import com.andersen.corgiapp.repository.UserRepositoryImpl;
+import com.andersen.corgiapp.service.UserService;
+import com.andersen.corgiapp.service.UserServiceImpl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "EditUserFormServlet", value = "/users/edit")
+public class EditUserFormServlet extends HttpServlet {
+
+    public static final String USER_EDIT_PATH = "/WEB-INF/jsp/editUser.jsp";
+
+    private static final String USER_PARAMETER = "user";
+    private static final String ID_PARAMETER = "id";
+
+    private final UserService userService;
+
+    public EditUserFormServlet() {
+        UserRepository userRepository = new UserRepositoryImpl();
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long userId = Long.parseLong(request.getParameter(ID_PARAMETER));
+        User user = userService.find(userId);
+
+        request.setAttribute(USER_PARAMETER, user);
+        request.getRequestDispatcher(USER_EDIT_PATH).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
